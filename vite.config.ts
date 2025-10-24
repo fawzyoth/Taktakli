f14758a739c0f4829d4f05bb9c0e0dd0b2caacaa
fawzyoth <50986338+fawzyoth@users.noreply.github.com>
2025-10-24 15:48:59 +0100

    Updated tailwind.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-});
+  server: {
+    port: 5173
+  }
+})
